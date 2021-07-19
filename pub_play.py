38395ef39bb09cc9afc7025eadd74e9d8cb85cbf
Francesco Anselmo <francesco.anselmo@gmail.com>
2021-07-19 16:07:31 +0200

    time offset and bugfix

--- pub_play.py
+++ pub_play.py
@@ -15,10 +15,14 @@ DMX = True
 # 18:59 Karaoke
 # 22:00 Documentary
 
+# HDMI 4
+
 # Xicato group is 102
 
 # Video:
 # /opt/video/Publicani\ Assembly\ 03.mp4
+
+# TODO: Add Xicato support
 
 from time import sleep, time
 from datetime import datetime, timedelta
@@ -35,10 +39,12 @@ dmx = None
 ipcon = None
 VIDEO_FILENAME = '/opt/video/Publicani_Assembly_03.mp4'
 AUDIO_VOLUME = '0'
-AUDIO_DEVICE = 'hdmi'
+AUDIO_DEVICE = 'both'
 AUDIO_LAYOUT = '2.1'
+AUDIO_OFFSET = '4'
 
 try:
+
     if environ['VIDEO_FILENAME'] != '':
         VIDEO_FILENAME = environ['VIDEO_FILENAME']
     else:
@@ -52,7 +58,7 @@ try:
     if environ['AUDIO_DEVICE'] != '':
         AUDIO_DEVICE = environ['AUDIO_DEVICE']
     else:
-        AUDIO_DEVICE = 'hdmi'
+        AUDIO_DEVICE = 'both'
         # options: alsa, hdmi
 
     if environ['AUDIO_LAYOUT'] != '':
@@ -60,6 +66,12 @@ try:
     else:
         AUDIO_LAYOUT = '2.1'
         # options: 2.1, 5.1, 7.1
+
+    if environ['AUDIO_OFFSET'] != '':
+        AUDIO_OFFSET = environ['AUDIO_OFFSET']
+    else:
+        AUDIO_OFFSET = '4'
+
 except:
     print("No environment variables")
 
@@ -83,51 +95,55 @@ def disco_ball_on():
 
 
 if __name__ == "__main__":
+    while True:
+        offset = int(AUDIO_OFFSET)
 
-    if DMX:
-        ipcon = IPConnection() # Create Tinkerforge IP connection
-        dmx = BrickletDMX(UID, ipcon) # Create device object
-        ipcon.connect(HOST, PORT) # Connect to brickd
-        # Don't use device before ipcon is connected
-        # Configure Bricklet as DMX master
-        dmx.set_dmx_mode(dmx.DMX_MODE_MASTER)
+        if DMX:
+            ipcon = IPConnection() # Create Tinkerforge IP connection
+            dmx = BrickletDMX(UID, ipcon) # Create device object
+            ipcon.connect(HOST, PORT) # Connect to brickd
+            # Don't use device before ipcon is connected
+            # Configure Bricklet as DMX master
+            dmx.set_dmx_mode(dmx.DMX_MODE_MASTER)
 
-    # Timecodes:
-    # 00:00 Documentary
-    # 00:50 Karaoke
-    # 04:01 Documentary
-    # 05:11 Karaoke
-    # 08:21 Documentary
-    # 18:59 Karaoke
-    # 22:00 Documentary
+        # Timecodes:
+        # 00:00 Documentary
+        # 00:50 Karaoke
+        # 04:01 Documentary
+        # 05:11 Karaoke
+        # 08:21 Documentary
+        # 18:59 Karaoke
+        # 22:00 Documentary
 
-    disco_ball_off()
+        disco_ball_off()
 
-    scheduler = BackgroundScheduler({'apscheduler.timezone': 'Europe/Rome'})
-    t = timedelta(seconds=50)
-    scheduler.add_job(disco_ball_on, 'date', run_date=datetime.now()+t)
-    t = timedelta(seconds=4*60+1)
-    scheduler.add_job(disco_ball_off, 'date', run_date=datetime.now()+t)
-    t = timedelta(seconds=5*60+11)
-    scheduler.add_job(disco_ball_on, 'date', run_date=datetime.now()+t)
-    t = timedelta(seconds=8*60+21)
-    scheduler.add_job(disco_ball_off, 'date', run_date=datetime.now()+t)
-    t = timedelta(seconds=18*60+59)
-    scheduler.add_job(disco_ball_on, 'date', run_date=datetime.now()+t)
-    t = timedelta(seconds=22*60+21)
-    scheduler.add_job(disco_ball_off, 'date', run_date=datetime.now()+t)
+        scheduler = BackgroundScheduler({'apscheduler.timezone': 'Europe/Rome'})
+        t = timedelta(seconds=50+offset)
+        scheduler.add_job(disco_ball_on, 'date', run_date=datetime.now()+t)
+        t = timedelta(seconds=4*60+1+offset)
+        scheduler.add_job(disco_ball_off, 'date', run_date=datetime.now()+t)
+        t = timedelta(seconds=5*60+11+offset)
+        scheduler.add_job(disco_ball_on, 'date', run_date=datetime.now()+t)
+        t = timedelta(seconds=8*60+21+offset)
+        scheduler.add_job(disco_ball_off, 'date', run_date=datetime.now()+t)
+        t = timedelta(seconds=18*60+59+offset)
+        scheduler.add_job(disco_ball_on, 'date', run_date=datetime.now()+t)
+        t = timedelta(seconds=22*60+21+offset)
+        scheduler.add_job(disco_ball_off, 'date', run_date=datetime.now()+t)
 
-    scheduler.start()
+        scheduler.start()
 
-    # TODO: add player
-    #sleep(30)
-    filename = VIDEO_FILENAME
-    print("Playing file:", filename)
-    system('omxplayer -r -b -o %s --vol %s --layout %s "%s"' % (AUDIO_DEVICE, str(AUDIO_VOLUME), str(AUDIO_LAYOUT), filename))
+        # TODO: add player
+        #sleep(30)
+        filename = VIDEO_FILENAME
+        print("Playing file:", filename)
+        cmd = 'omxplayer -r -b -o %s --vol %s --layout %s "%s"' % (AUDIO_DEVICE, str(AUDIO_VOLUME), str(AUDIO_LAYOUT), filename)
+        print(cmd)
+        system(cmd)
 
-    # Shutdown the scheduler
-    scheduler.shutdown()
+        # Shutdown the scheduler
+        scheduler.shutdown()
 
-    if DMX:
-        # Disconnect the Tinkerforge connection
-        ipcon.disconnect()
+        if DMX:
+            # Disconnect the Tinkerforge connection
+            ipcon.disconnect()
